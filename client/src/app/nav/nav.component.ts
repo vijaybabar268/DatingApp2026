d5f828ab1c09642388560bbbf0f0a4c666108d33
Vijaybabar <babarvm@gmail.com>
2026-03-29 16:35:54 +0530

    Login & Register - Subscribe and Unsubscribe using async pipe

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
 
 @Component({
   selector: 'app-nav',
@@ -8,26 +10,18 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  loggedIn = false;
+  currentUser$: Observable<User | null> = of(null);
 
   constructor(private accountService: AccountService) { }
 
   ngOnInit() {
-    this.getCurrentUser();
-  }
-
-  getCurrentUser() {
-    this.accountService.currentUser$.subscribe({
-      next: (user) => this.loggedIn = !!user,
-      error: (err) => console.log(err)
-    });
+    this.currentUser$ = this.accountService.currentUser$;
   }
 
   login() {
     this.accountService.login(this.model).subscribe({
       next: (res) => {
         console.log("Success");
-        this.loggedIn = true;
       },
       error: (err) => console.log("Error: ", err),
       complete: () => {
@@ -39,7 +33,6 @@ export class NavComponent implements OnInit {
 
   logout() {
     this.accountService.logout();
-    this.loggedIn = false;
   }
 
 }
