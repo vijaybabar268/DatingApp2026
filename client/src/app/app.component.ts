7a188c474321a2980ca6f820575370c9ffbb2ee8
Vijaybabar <babarvm@gmail.com>
2026-03-29 14:26:54 +0530

    Login & Register - Created nav component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -7,7 +7,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title: string = 'Dating app';
   users: any;
 
   constructor(private http: HttpClient) { }
