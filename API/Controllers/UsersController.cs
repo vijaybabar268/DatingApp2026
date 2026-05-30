b87bc3efbd8484d3269da18b4b513e6e3bd988d2
Vijaybabar <babarvm@gmail.com>
2026-05-30 22:17:04 +0530

    Added automapper

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,6 +1,8 @@
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,21 +12,32 @@ namespace API.Controllers;
 public class UsersController : BaseApiController
 {
     private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
 
-    public UsersController(IUserRepository userRepository)
+    public UsersController(IUserRepository userRepository, IMapper mapper)
     {
         _userRepository = userRepository;
+        _mapper = mapper;
     }
 
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
     {
-        return Ok(await _userRepository.GetUsersAsync());
+        var users = await _userRepository.GetUsersAsync();
+
+        var usersToReturn = _mapper.Map<IEnumerable<AppUser>, IEnumerable<MemberDto>>(users);
+
+        return Ok(usersToReturn);
     }
         
     [HttpGet("{username}")]
-    public async Task<ActionResult<AppUser>> GetUsers(string username)
+    public async Task<ActionResult<MemberDto>> GetUsers(string username)
     {
-        return Ok(await _userRepository.GetUserByUsernameAsync(username));
+        var user = await _userRepository.GetUserByUsernameAsync(username);
+
+        var userToReturn = _mapper.Map<AppUser,MemberDto>(user);
+
+        return Ok(userToReturn);
+
     }
 }
